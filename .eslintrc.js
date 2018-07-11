02643c30b857f6a1280395054cbc26ab5a88a652
Doran Walsten <doran.walsten@gmail.com>
2018-07-11 13:20:46 -0400

    Setup package, config, build files

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+	"extends": "airbnb-base",
+	"plugins": [
+		"import"
+	],
+	"rules": {
+		"indent": ["error", "tab", {"SwitchCase": 1}],
+		"no-console": ["error", { "allow": ["error"]}],
+		"no-tabs": ["off"],
+		"comma-dangle": ["off"],
+		"import/extensions": ["off"],
+		"import/no-unresolved": [2, {
+			ignore: ['config']
+		}],
+		"import/no-extraneous-dependencies": "off",
+		"comma-dangle": [1, "always-multiline"]
+	},
+	"globals": {
+		"document": true,
+		"config": true,
+		"navigator": true,
+		"VERSION": true,
+		"window": true,
+	},
+	"settings": {
+		"import/ignore": ["config"]
+	},
+	"env": {
+		"jest": true,
+	},
+};
